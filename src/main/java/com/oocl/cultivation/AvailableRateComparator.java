c454d0c5b236d05fb9b4d3f8a333eb70ffd2c6ca
theroadof <1294911641@qq.com>
2020-07-25 11:22:14 +0800

    [ParkingLot][Teddy] feature:implement should_park_car_in_larger_available_position_rate_parking_lot_when_park_given_car

---
+++ src/main/java/com/oocl/cultivation/AvailableRateComparator.java
@@ -0,0 +1,15 @@
+package com.oocl.cultivation;
+
+import java.util.Comparator;
+
+public class AvailableRateComparator implements Comparator<ParkingLot> {
+
+    @Override
+    public int compare(ParkingLot o1, ParkingLot o2) {
+        if((o1.getCapacity()-o1.getParkingLot().size())/o1.getCapacity() < (o2.getCapacity()-o2.getParkingLot().size())/o2.getCapacity()){
+            return 1;
+        }else {
+            return -1;
+        }
+    }
+}
--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -1,8 +1,22 @@
 package com.oocl.cultivation;
 
+import java.util.Collections;
+
 public class SuperSmartParkingBoy extends ParkingBoy{
+    public SuperSmartParkingBoy() {
+        super();
+    }
+
     @Override
     public CarTicket park(Car car) {
-        return null;
+        Collections.sort(this.getParkingLots(),new AvailableRateComparator());
+        CarTicket carTicket = new CarTicket();
+        this.currentParkingLot = this.getParkingLots().get(0);
+        if (this.currentParkingLot.getParkingLot().size() >= this.currentParkingLot.getCapacity()) {
+            this.respondMessage = "Not enough position.";
+            return null;
+        }
+        this.currentParkingLot.getParkingLot().put(carTicket, car);
+        return carTicket;
     }
 }
