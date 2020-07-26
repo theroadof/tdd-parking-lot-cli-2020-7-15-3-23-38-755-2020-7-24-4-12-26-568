f33ae73bd710310b72ad2e7957d2cf2d5108436c
theroadof <1294911641@qq.com>
2020-07-26 09:58:29 +0800

    [ParkingLot][Teddy] refactor:add SuperSmartParkingBoyTest and move should_park_car_in_larger_available_position_rate_parking_lot_when_park_given_car test case to SuperSmartParkingBoyTest

---
+++ src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
@@ -0,0 +1,30 @@
+package com.oocl.cultivation.test;
+
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.CarTicket;
+import com.oocl.cultivation.SuperSmartParkingBoy;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SuperSmartParkingBoyTest {
+    @Test
+    void should_park_car_in_larger_available_position_rate_parking_lot_when_park_given_car() {
+        //given
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy();
+        Car car = new Car();
+        for (int i = 0; i < 5; i++) {
+            if (i % 2 == 0) {
+                superSmartParkingBoy.getParkingLots().get(1).getParkingLot().put(new CarTicket(), new Car());
+            }
+            superSmartParkingBoy.getParkingLots().get(0).getParkingLot().put(new CarTicket(), new Car());
+        }
+
+        //when
+        int size = superSmartParkingBoy.getParkingLots().get(1).getParkingLot().size();
+        superSmartParkingBoy.park(car);
+
+        //then
+        assertEquals(size + 1, superSmartParkingBoy.getCurrentParkingLot().getParkingLot().size());
+    }
+}
